b54a83e3afada80e215b429374dba65a070b48f3
Brian Keyonzo <briankeyonzo@yahoo.com>
2017-12-24 18:43:07 +0300

    Setting up theme

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+require('./app.css');
+
+ReactDOM.render(
+    "Hello",document.getElementById('app'));
+
---
+++ frontend/webpack.config.js
@@ -0,0 +1,49 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CompressionPlugin = require("compression-webpack-plugin");
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + '/build',
+        filename: "main.js"
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css'),
+        new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.(js|html)$/,
+			threshold: 10240,
+			minRatio: 0.8
+		}),
+         new webpack.DefinePlugin({
+             'process.env.NODE_ENV': '"production"'
+        }),
+    ],
+    // devtool: "#eval-source-map",
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    "react",
+                    ["es2015", { modules: false }],
+                    "stage-1",
+                ],
+                },
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg|eot|svg|ttf|woff|woff2)$/,
+            loader: "file-loader"
+        },
+        {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }
+            )
+        }
+    ]
+    }
+};
