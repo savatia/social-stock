b64b009c872f642d6e1d90ab129d7072f66c15b4
Brian Keyonzo <briankeyonzo@yahoo.com>
2018-01-12 17:21:26 +0300

    Added realy and some filters

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,20 +1,37 @@
 import graphene
+from graphene import relay
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+
 from .models import *
 
-class SubSectorType(DjangoObjectType):
+class SubSectorNode(DjangoObjectType):
 	class Meta:
 		model = SubSector
+		filter_fields = {
+			'name': ['exact', 'icontains', 'istartswith'],
+		}
+
+		interfaces = (relay.Node, )
 
 
-class CompanyType(DjangoObjectType):
+class CompanyNode(DjangoObjectType):
 	class Meta:
 		model = Company
+		filter_fields = {
+			'name': ['exact', 'icontains', 'istartswith'],
+			'symbol': ['exact', 'icontains', 'istartswith'],
+			'country': ['exact', 'icontains', 'istartswith'],
+		}
+		interfaces = (relay.Node,)
 
 
 class Query(object):
-	companies = graphene.List(CompanyType)
-	sub_sectors = graphene.List(SubSectorType)
+	company = relay.Node.Field(CompanyNode)
+	companies = DjangoFilterConnectionField(CompanyNode)
+
+	sub_sector = relay.Node.Field(SubSectorNode)
+	sub_sectors = DjangoFilterConnectionField(SubSectorNode)
 
 	def resolve_companies(self, info, **kwargs):
 		return Company.objects.all()
